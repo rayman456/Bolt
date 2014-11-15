fb5c41b51da54b20c669d7e5eecb300e0ba351c1
Rutger Yager <rutger.yager@gmail.com>
2014-11-15 08:21:09 -0600

    Revert "Test"
    This reverts commit b0b5cc158caefa04010d441c9c90a05786ab5de4.

--- src/com/example/bolt/MainActivity.java
+++ src/com/example/bolt/MainActivity.java
@@ -18,7 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-//Im making a change
+
 public class MainActivity extends Activity implements ActionBar.TabListener {
 
     /**
