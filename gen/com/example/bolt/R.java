b48d31e79019de25d4efbac2e9668c338d553812
Nicholas Arauza <narauza@gmail.com>
2014-11-23 18:52:46 -0600

    fixed warnings

--- gen/com/example/bolt/R.java
+++ gen/com/example/bolt/R.java
@@ -57,11 +57,21 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050005;
         public static final int app_name=0x7f050000;
+        public static final int calories_burned=0x7f05000a;
+        public static final int distance=0x7f05000b;
         public static final int hello_world=0x7f050004;
+        public static final int instructions=0x7f05000d;
+        public static final int password_hint=0x7f05000f;
+        public static final int pause_run=0x7f050009;
+        public static final int running_time=0x7f050007;
+        public static final int start_run=0x7f050008;
         public static final int title_activity_bolt_login=0x7f050006;
         public static final int title_section1=0x7f050001;
         public static final int title_section2=0x7f050002;
         public static final int title_section3=0x7f050003;
+        public static final int user_hint=0x7f05000e;
+        public static final int user_login=0x7f050010;
+        public static final int welcome=0x7f05000c;
     }
     public static final class style {
         /** 
