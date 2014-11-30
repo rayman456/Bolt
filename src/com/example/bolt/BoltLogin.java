e064bf0ce82020c2d2d7fbf7727ebf9f54a4fbc0
Nicholas Arauza <narauza@gmail.com>
2014-11-30 16:27:18 -0600

    fixed my mistakes :)

--- src/com/example/bolt/BoltLogin.java
+++ src/com/example/bolt/BoltLogin.java
@@ -1,14 +1,10 @@
 package com.example.bolt;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.content.Intent;
 
 public class BoltLogin extends Activity
